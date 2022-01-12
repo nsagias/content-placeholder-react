fdb4259b6ab6331888af1a69336eea8ada3d6b75
Nick <nsagias@gmail.com>
2022-01-12 00:51:30 -0500

    remove content from html and add it to getdata mock function

--- original/script.js
+++ original/script.js
@@ -11,10 +11,10 @@ const animated_bg_texts = document.querySelectorAll("animated_bg_text");
 function getData() {
   header.innerHTML = `<img src="https://images.unsplash.com/photo-1484788984921-03950022c9ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1832&q=80" alt=""
       />`;
-  title.innerHTML = "";
-  excerpt.innerHTML = "";
-  profile_img.innerHTML = "";
-  name.innerHTML = "";
-  date.innerHTML = "";
+  title.innerHTML = "Lorem ipsum dolor sit amet.";
+  excerpt.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, nesciunt.";
+  profile_img.innerHTML = `<img src="https://randomuser.me/api/portraits/women/45.jpg" alt="" />`;
+  name.innerHTML = "Jane Doe";
+  date.innerHTML = "Jan 12, 2022";
    
 }
