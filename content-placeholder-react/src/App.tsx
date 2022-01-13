e3d17b38fd33de80d36b2ba77626dd5bfdb786c5
Nick <nsagias@gmail.com>
2022-01-12 21:49:19 -0500

    import html and convert class to className

--- content-placeholder-react/src/App.tsx
+++ content-placeholder-react/src/App.tsx
@@ -5,6 +5,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      <div className="card">
+          <div className="card-header animated-bg" id="header">
+            &nbsp;
+          </div>
+
+          <div className="card-content">
+            <h3 className="card-title animated-bg animated-bg-text" id="title">&nbsp;</h3>
+            <p className="card-excerpt" id="excerpt">
+            &nbsp;
+              <span className="animated-bg animated-bg-text">&nbsp;</span>
+              <span className="animated-bg animated-bg-text">&nbsp;</span>
+              <span className="animated-bg animated-bg-text">&nbsp;</span>
+            </p>
+            <div className="author">
+              <div className="profile-img animated-bg" id="profile_img">
+                &nbsp;
+              </div>
+              <div className="author-info">
+                <strong className="animated-bg animated-bg-text" id="name">&nbsp;</strong>
+                <small className="animated-bg animated-bg-text" id="date">&nbsp;</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
     </div>
   );
 }
